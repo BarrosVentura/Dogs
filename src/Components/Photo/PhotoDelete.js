fe7b11d9702813312060982aa157ead268de09bc
Adson Ventura <adsonventura@gmail.com>
2020-08-27 07:59:42 -0300

    Foto Delete

---
+++ src/Components/Photo/PhotoDelete.js
@@ -0,0 +1,33 @@
+import React from "react";
+import style from "./PhotoDelete.module.css";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTO_DELETE } from "../../api";
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  async function handleClick() {
+    const confirm = window.confirm("Tem certezsa que deseja deletar?");
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) window.location.reload();
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button className={StyleSheet.delete} disabled>
+          Deletar
+        </button>
+      ) : (
+        <button onClick={handleClick} className={StyleSheet.delete}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;
